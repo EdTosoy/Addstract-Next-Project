0a464928c83e4da4e6612f5116b5843eeb31e92d
EdTosoy <edberto.tosoy@gmail.com>
2021-02-02 13:02:07 +0800

    added dependencies

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>Addstract</title>
+        <meta
+          name="description"
+          content="Our menu is constantly evolving, yet always abides by a few simple constants. All of our cooking revolves around open wood fire. We work with a small group of fisherman, hunters, gatherers, ranchers, and farmers to find and follow microclimates that produce specific tastes from wild foods. Lastly, we steer clear of imported and commercial goods, opting instead to build our own pantry through patient harvesting and preservation."
+        />
+      </Head>
+    </div>
+  );
+}
